2218cd8d50538d3a55ed5bd4db5f446cd047d31c
Ivan Miletic <milemik68@gmail.com>
2023-11-20 23:14:49 +0100

    Added test for card.min

--- card/card_test.go
+++ card/card_test.go
@@ -29,3 +29,25 @@ func Test_responseOK(t *testing.T) {
 		}
 	}
 }
+
+func Test_min(t *testing.T) {
+	minTestCases := []struct {
+		firstVal  byte
+		secondVal byte
+		expected  byte
+	}{
+		{firstVal: 0, secondVal: 0, expected: 0},
+		{firstVal: 0x0F, secondVal: 0x1F, expected: 0x0F},
+		{firstVal: 0x0F, secondVal: 0x0F, expected: 0x0F},
+		{firstVal: 0x0F, secondVal: 0x0C, expected: 0x0C},
+	}
+
+	for _, testVal := range minTestCases {
+		res := min(int(testVal.firstVal), int(testVal.secondVal))
+
+		if res != int(testVal.expected) {
+			t.Errorf("Got %v and we expect %v", res, testVal.expected)
+		}
+	}
+
+}
