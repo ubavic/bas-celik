8cdd5ffa2d2790d03fa2070b9a58261ee9dbc84d
Ivan Miletic <milemik68@gmail.com>
2023-11-21 13:04:18 +0100

    Removed TestReadCard test

--- card/card_test.go
+++ card/card_test.go
@@ -2,18 +2,7 @@ package card
 
 import (
 	"testing"
-
-	"github.com/ebfe/scard"
 )
-
-func TestReadCardInit(t *testing.T) {
-	var card scard.Card
-	_, err := ReadCard(&card)
-
-	if err == nil {
-		t.Errorf("Expected error here!")
-	}
-}
 
 func Test_responseOK(t *testing.T) {
 	byteSliceTest := []struct {
