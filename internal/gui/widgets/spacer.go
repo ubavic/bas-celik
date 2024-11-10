8b280e7a73e97510ebd1aef650b87ede9c8a00e0
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2024-11-11 00:39:50 +0100

    Implement spacer minimal width

--- internal/gui/widgets/spacer.go
+++ internal/gui/widgets/spacer.go
@@ -8,6 +8,7 @@ import (
 
 type Spacer struct {
 	widget.BaseWidget
+	minWidth float32
 }
 
 type SpacerRenderer struct {
@@ -18,6 +19,14 @@ func NewSpacer() *Spacer {
 	spacer := &Spacer{}
 	spacer.ExtendBaseWidget(spacer)
 	return spacer
+}
+
+func (s *Spacer) SetMinWidth(width float32) {
+	if width < 0 {
+		width = 0
+	}
+
+	s.minWidth = width
 }
 
 func (s *Spacer) CreateRenderer() fyne.WidgetRenderer {
@@ -31,7 +40,13 @@ func (r *SpacerRenderer) Refresh() {}
 func (r *SpacerRenderer) Layout(s fyne.Size) {}
 
 func (r *SpacerRenderer) MinSize() fyne.Size {
-	return fyne.NewSize(theme.Padding(), 5*theme.Padding())
+	width := r.spacer.minWidth
+
+	if r.spacer.minWidth == 0 {
+		width = theme.Padding()
+	}
+
+	return fyne.NewSize(width, 5*theme.Padding())
 }
 
 func (r *SpacerRenderer) Objects() []fyne.CanvasObject {
