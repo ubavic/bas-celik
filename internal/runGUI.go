5bf19e379a3bee1e828854a487b6530d07fdad03
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2025-12-20 22:41:44 +0100

    Remove unused icons

--- internal/runGUI.go
+++ internal/runGUI.go
@@ -4,7 +4,6 @@ package internal
 
 import (
 	"github.com/ubavic/bas-celik/v2/internal/gui"
-	"github.com/ubavic/bas-celik/v2/internal/gui/icon"
 	"github.com/ubavic/bas-celik/v2/internal/gui/translation"
 	"github.com/ubavic/bas-celik/v2/internal/logger"
 )
@@ -12,11 +11,6 @@ import (
 func Run(cfg LaunchConfig) error {
 	if len(cfg.PdfPath) == 0 && len(cfg.JsonPath) == 0 && len(cfg.ExcelPath) == 0 {
 		err := translation.SetTranslations(cfg.EmbedDirectory)
-		if err != nil {
-			return err
-		}
-
-		err = icon.LoadIcons(cfg.EmbedDirectory)
 		if err != nil {
 			return err
 		}
