7e58434037bfc098c12c8a2eaee32fc4c852b745
r4start <r4start@protonmail.ch>
2026-04-10 11:08:53 +0200

    Update `DetectCardDocumentByAtr` to include `GEMALTO_ATR_4` for medical insurance.

--- card/atr.go
+++ card/atr.go
@@ -18,10 +18,8 @@ func (atr Atr) Is(otherAtr Atr) bool {
 func DetectCardDocumentByAtr(atr Atr) []CardDocumentType {
 	if atr.Is(GEMALTO_ATR_1) {
 		return []CardDocumentType{GemaltoIdDocumentCardType, VehicleDocumentCardType}
-	} else if atr.Is(GEMALTO_ATR_2) || atr.Is(GEMALTO_ATR_3) {
+	} else if atr.Is(GEMALTO_ATR_2) || atr.Is(GEMALTO_ATR_3) || atr.Is(GEMALTO_ATR_4) {
 		return []CardDocumentType{GemaltoIdDocumentCardType, MedicalDocumentCardType, VehicleDocumentCardType}
-	} else if atr.Is(GEMALTO_ATR_4) {
-		return []CardDocumentType{GemaltoIdDocumentCardType, VehicleDocumentCardType}
 	} else if atr.Is(MEDICAL_ATR_1) || atr.Is(MEDICAL_ATR_2) {
 		return []CardDocumentType{MedicalDocumentCardType}
 	} else if atr.Is(VEHICLE_ATR_0) || atr.Is(VEHICLE_ATR_2) || atr.Is(VEHICLE_ATR_3) || atr.Is(VEHICLE_ATR_4) {
